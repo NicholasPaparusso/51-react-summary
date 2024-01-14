757f2690d2fcd487c6df48f9036885bc99a553b7
NicholasPaparusso <nicopaparusso@gmail.com>
2024-01-14 16:20:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+      <div>
+            <h1>Hello World!</h1>
+            <Post />
+      </div>
+    );
+}
+
+export default App;
